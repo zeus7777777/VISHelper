bf916834b74571487d0222e5db4d3e03ceec0736
skgg <sianchen.kevin@gmail.com>
2016-08-25 15:28:01 +0800

    UI of match info

---
+++ app/src/main/java/com/mixerbox/hackathon/vis/MatchInfo.java
@@ -0,0 +1,13 @@
+package com.mixerbox.hackathon.vis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MatchInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_info);
+    }
+}
