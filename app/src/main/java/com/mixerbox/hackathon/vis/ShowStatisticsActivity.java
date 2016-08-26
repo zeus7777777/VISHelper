02fed8e328eca42fcb88c527b0e9502f77ac0c77
zeus7777777 <zeus840703@gmail.com>
2016-08-26 08:14:31 +0800

    add html

--- app/src/main/java/com/mixerbox/hackathon/vis/ShowStatisticsActivity.java
+++ app/src/main/java/com/mixerbox/hackathon/vis/ShowStatisticsActivity.java
@@ -1,13 +1,21 @@
 package com.mixerbox.hackathon.vis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -45,5 +53,54 @@ public class ShowStatisticsActivity extends AppCompatActivity {
 
 
         Log.d("hmsize", hm.size()+"");
+        try {
+            Log.d("base", getBase64());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBase64() throws JSONException {
+        JSONArray ja = new JSONArray();
+        String[] keys = hm.keySet().toArray(new String[1]);
+        for(int i=0;i<hm.size();i++)
+        {
+            JSONObject jo = new JSONObject();
+            jo.put("name", keys[i]);
+            JSONArray arr = new JSONArray();
+            for(int j=0;j<18;j++)
+                arr.put(hm.get(keys[i]).result[j]);
+            jo.put("result", arr);
+            ja.put(jo);
+        }
+        String ans = Base64.encodeToString(ja.toString().getBytes(), Base64.DEFAULT);
+        return ans;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sharemenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.act_share)
+        {
+            Intent it = new Intent();
+            it.setAction(Intent.ACTION_SEND);
+            try {
+                it.putExtra(Intent.EXTRA_TEXT, "http://140.119.19.32/table.html?base64="+getBase64());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            it.setType("text/plain");
+            startActivity(Intent.createChooser(it, "share content by"));
+            return true;
+        }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
